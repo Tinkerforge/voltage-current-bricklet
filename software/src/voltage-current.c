97335ae2100f74ead789e7cf82e929f7b9e87e6d
Olaf Lüke <olaf@tinkerforge.com>
2012-12-18 14:40:35 +0100

    Add sanity checks for calibration and configuration
    Conflicts:
    
    	software/src/voltage-current.c

--- software/src/voltage-current.c
+++ software/src/voltage-current.c
@@ -129,7 +129,11 @@ void destructor(void) {
 void tick(const uint8_t tick_type) {
     if(!(BC->pin_alert->pio->PIO_PDSR & BC->pin_alert->mask)) {
     	if(BA->mutex_take(*BA->mutex_twi_bricklet, 10)) {
-    		BC->value[SIMPLE_UNIT_CURRENT] = ina226_read_register(INA226_REG_CURRENT)*BC->gain_muldiv[0]/BC->gain_muldiv[1];
+    		if(BC->gain_muldiv[1] != 0) {
+    			BC->value[SIMPLE_UNIT_CURRENT] = ina226_read_register(INA226_REG_CURRENT)*BC->gain_muldiv[0]/BC->gain_muldiv[1];
+    		} else {
+    			BC->value[SIMPLE_UNIT_CURRENT] = ina226_read_register(INA226_REG_CURRENT);
+    		}
     		BC->value[SIMPLE_UNIT_VOLTAGE] = ina226_read_register(INA226_REG_BUS_VOLTAGE)*5/4;
 
     		// clear alert pin
@@ -246,9 +250,9 @@ void get_configuration(const ComType com, const GetConfiguration *data) {
 	gcr.stack_id                = data->stack_id;
 	gcr.type                    = data->type;
 	gcr.length                  = sizeof(GetConfigurationReturn);
-	gcr.averaging               = BC->averaging;
-	gcr.current_conversion_time = BC->current_conversion_time;
-	gcr.voltage_conversion_time = BC->voltage_conversion_time;
+	gcr.averaging               = MIN(BC->averaging, 7);
+	gcr.current_conversion_time = MIN(BC->current_conversion_time, 7);
+	gcr.voltage_conversion_time = MIN(BC->voltage_conversion_time, 7);
 
 	BA->send_blocking_with_timeout(&gcr,
 	                               sizeof(GetConfigurationReturn),
