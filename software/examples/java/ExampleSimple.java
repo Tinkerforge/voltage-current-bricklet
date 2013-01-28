be0ef3712807d1bb111652ebe9a0f8ab5840ed3e
Matthias Bolte <matthias@tinkerforge.com>
2013-01-28 13:19:19 +0100

    Update outdated comment in Java example

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -15,10 +15,9 @@ public class ExampleSimple {
 		ipcon.connect(host, port); // Connect to brickd
 		// Don't use device before ipcon is connected
 
-
 		// Get current current and voltage (unit is mA and mV)
-		int current = vc.getCurrent(); // Can throw IPConnection.TimeoutException
-		int voltage = vc.getVoltage(); // Can throw IPConnection.TimeoutException
+		int current = vc.getCurrent(); // Can throw com.tinkerforge.TimeoutException
+		int voltage = vc.getVoltage(); // Can throw com.tinkerforge.TimeoutException
 
 		System.out.println("Current: " + current/1000.0 + " A");
 		System.out.println("Voltgae: " + voltage/1000.0 + " V");
