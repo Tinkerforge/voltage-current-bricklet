1eda6363978fcabf2aa938820f7a55365efae6a9
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:41:28 +0200

    java: Unify constants style

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -2,8 +2,8 @@ import com.tinkerforge.BrickletVoltageCurrent;
 import com.tinkerforge.IPConnection;
 
 public class ExampleSimple {
-	private static final String host = "localhost";
-	private static final int port = 4223;
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
@@ -12,7 +12,7 @@ public class ExampleSimple {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletVoltageCurrent vc = new BrickletVoltageCurrent(UID, ipcon); // Create device object
 
-		ipcon.connect(host, port); // Connect to brickd
+		ipcon.connect(HOST, PORT); // Connect to brickd
 		// Don't use device before ipcon is connected
 
 		// Get current current and voltage (unit is mA and mV)
