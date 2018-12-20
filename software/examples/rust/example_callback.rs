f3a32e62732ba9205e7b1db3b9e5ed4914c38cc7
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:42:26 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, voltage_current_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  voltage_current_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let vc = VoltageCurrentBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let current_receiver = vc.get_current_callback_receiver();
+     let current_receiver = vc.get_current_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `vc` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for current in current_receiver {
-            println!("Current: {} A", current as f32 / 1000.0);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `vc` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for current in current_receiver {           
+                		println!("Current: {} A", current as f32 /1000.0);
+            }
+        });
 
-    // Set period for current receiver to 1s (1000ms).
-    // Note: The current callback is only called every second
-    //       if the current has changed since the last call!
-    vc.set_current_callback_period(1000);
+		// Set period for current receiver to 1s (1000ms).
+		// Note: The current callback is only called every second
+		//       if the current has changed since the last call!
+		vc.set_current_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, voltage_current_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  voltage_current_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,15 +13,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let vc = VoltageCurrentBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get current voltage.
-    let voltage = vc.get_voltage().recv()?;
-    println!("Voltage: {} V", voltage as f32 / 1000.0);
+		// Get current voltage.
+let voltage = vc.get_voltage().recv()?;
+		println!("Voltage: {} V", voltage as f32 /1000.0);
 
-    // Get current current.
-    let current = vc.get_current().recv()?;
-    println!("Current: {} A", current as f32 / 1000.0);
+		// Get current current.
+let current = vc.get_current().recv()?;
+		println!("Current: {} A", current as f32 /1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
